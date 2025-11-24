9446f6c76eeeb9dab8c9285791ee06c73c75e595
Rangerboy574 <jdeweese574@icloud.com>
2025-11-24 14:39:09 -0600

    Initial Commit

---
+++ QuickThink/QuickThinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickThinkApp.swift
+//  QuickThink
+//
+//  Created by Joseph DeWeese on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct QuickThinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ QuickThinkTests/QuickThinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  QuickThinkTests.swift
+//  QuickThinkTests
+//
+//  Created by Joseph DeWeese on 11/24/25.
+//
+
+import Testing
+@testable import QuickThink
+
+struct QuickThinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
